5764e18839f57543cf9279c80a1cf46d76506dce
Federico Piñan <85468675+federicopinan@users.noreply.github.com>
2022-11-09 00:25:31 -0300

    Menu de Juegos
    Diseño del menu de juegos del programa, muestra la lista de juegos y opción para cerrar el programa

---
+++ menuDeJuegos.py
@@ -0,0 +1,35 @@
+import os
+#MENU SELECCIONADOR DE JUEGOS
+selecionarJuego = 0
+while selecionarJuego != 5:
+    print("▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲")
+    print("◀ Bienvenido a Play 4 in 1🕹️")
+    print("▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼")
+    print("1 ▷Adivina el número 🚀")
+    print("2 ▷Tablas de multiplicación 🚀")
+    print("3 ▷Piedra, paperl o tijera 🚀")
+    print("4 ▷Ahorcado 🚀")
+    print("5 ▷Salir del programa ❌")
+    selecionarJuego = int(input("Ingresa una opción\n..."))
+    os.system("cls")
+
+    #ENTRADA PARA EL PRIMER JUEGO
+    if selecionarJuego == 1:
+        os.system("cls")    
+    #ENTRADA PARA EL SEGUNDO JUEGO
+    if selecionarJuego == 2:
+        os.system("cls")
+    #ENTRDADA PARA EL TERCER JUEGO
+    if selecionarJuego == 3:
+        os.system("cls")
+    #ENTRADA PARA EL CUARTO JUEGO
+    if selecionarJuego == 4:
+        os.system("cls")
+    
+#FINALIZACIÓN DEL PROGRAMA
+os.system("cls" )
+print("")
+print("###################################")
+print("Finalizaste el programa!!")
+print("Gracias por jugar en Play 4 in 1 😜")
+print("###################################")
