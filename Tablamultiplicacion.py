c71a8e3222dfcb7be66fc052aa7569d9ce6dc7c8
NachoMansilla2103 <117238991+NachoMansilla2103@users.noreply.github.com>
2022-11-16 17:41:29 -0300

    Actualizacion 1, tablademultiplicacion.py
    cambio de condicionales
    eliminacion de true and break

--- Tablamultiplicacion.py
+++ Tablamultiplicacion.py
@@ -1,14 +1,17 @@
+print("Bienvenido a nuestro juego🎉")
+print("El juego se trata, de que aprendas las tablas de multiplicar de los numeros que deseas")
 num=int(input("Ingrese un numero para saber su tabla\n"))
+respuesta=""
 for i in range(1,11):
     total=num*i
     print(str(num)+"x"+str(i)+"="+str(total))
-while True:
-    respuesta=str(input("Desea Continuar?\n"))
+respuesta=str(input("(si),para continuar🤯 \n"))
+while respuesta=="si":
     if respuesta.lower()=="si":
         num=int(input("Ingrese un numero para saber su tabla\n"))
         for i in range(1,11):
-            total=num
+            total=num*i
             print(str(num)+"x"+str(i)+"="+str(total))
-    else:
-        print("Finalizo")
-        break
+    respuesta=str(input("Desea Continuar?\n"))
+else:
+        print("Finalizo 👌")
